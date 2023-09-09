c1c36e97ee2ae4d371a8046dd55f278fef24b86c
JaredLove <lovejared91@Yahoo.com>
2023-09-09 06:28:32 -0500

    added path

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
